9c40cd354b6f8747eac4da54d00374fbdd6efb4e
Charly77955 <desantiscarlos95@gmail.com>
2024-08-20 01:10:13 -0300

    otro commir

--- src/pages/components/products/allProducts.jsx
+++ src/pages/components/products/allProducts.jsx
@@ -4,17 +4,26 @@ import "../products/allProductsStyles.css";
 
 function AllProducts() {
   return (
-    <div classname="flex-container">
-        <div className="left-column">
-          <div classname="col-left">Filters</div>
-        </div>
-        <div className="right-column">
-          <div classname="col-right">
-            <ItemsContainer />
-          </div>
-        </div>
-      </div>
-    
+    // <div classname="flex-container">
+    //   <div className="container">
+
+      
+    //     <div className="left-column">
+    //       <div classname="col-left">Filters</div>
+    //     </div>
+    //     <div className="right-column">
+    //       <div classname="col-right">
+    //         <ItemsContainer />
+    //       </div>
+    //     </div>
+    //   </div>
+    //   </div>
+    <div class="flex-container">
+   <div class="Filters-container">Filters</div>
+   <div class="Items-container">
+    <div className="Items"><ItemsContainer />
+    </div></div>
+</div>
   );
 }
 
