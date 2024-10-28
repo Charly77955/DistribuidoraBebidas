145028f582f983154d017acb0e28f948f96383f2
Charly77955 <desantiscarlos95@gmail.com>
2024-10-27 23:22:10 -0300

    cambios para hacer landing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
+import "./App.css";
+import NavBar from "./pages/components/navbar/navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Products from "./pages/screens/products/Products";
 
-import './App.css';
-import NavBar from './pages/components/navbar/navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Products from './pages/screens/products/Products';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Inicio from './pages/screens/main/inicio';
+import Inicio from "./pages/screens/main/inicio";
 
 function App() {
   return (
     <div className="App">
-      <>
-       <BrowserRouter>
-       <NavBar/>
-       <Inicio/>
-      <Routes>
       
-        <Route path='Inicio' element={<Inicio/>}/>
-        <Route path='productos' element={<Products/>}/>
-        </Routes>
-        </BrowserRouter>
-        </>
-    </div>
+        <div class="flex-container">
+   <div class="flex-items"></div>
+   <div class="flex-items"></div>
+   <div class="flex-items"></div>
+   <div class="flex-items"></div>
+        </div>
+        </div>
   );
 }
 
